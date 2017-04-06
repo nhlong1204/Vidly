64bd70d4006ceae2148b632486aebfb98873654d
nhlong1204 <nhlong1204@gmail.com>
2017-04-06 13:41:42 +0900

    [Cty] Xong section 2

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,23 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id=1,Name="Shrek"},
+                new Movie{Id=2,Name="Wall-e"}
+
+            };
+        }
+
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
